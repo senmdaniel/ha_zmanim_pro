82364dbb5289647df45ad1799c0916c5a9c36a52
senmdaniel <senmdaniel@hotmail.com>
2026-05-28 14:31:53 +0200

    Update __init__.py

--- custom_components/ha_zmanim_pro/__init__.py
+++ custom_components/ha_zmanim_pro/__init__.py
@@ -1,1 +1,30 @@
+```python
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
+PLATFORMS = ["sensor"]
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+) -> bool:
+
+    await hass.config_entries.async_forward_entry_setups(
+        entry,
+        PLATFORMS,
+    )
+
+    return True
+
+
+async def async_unload_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+) -> bool:
+
+    return await hass.config_entries.async_unload_platforms(
+        entry,
+        PLATFORMS,
+    )
+```
