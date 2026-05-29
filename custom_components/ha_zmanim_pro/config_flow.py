b06419d5d03ea9b9d6f668617caeb7e40bd14a77
senmdaniel <senmdaniel@hotmail.com>
2026-05-29 10:26:53 +0200

    Update config_flow.py

--- custom_components/ha_zmanim_pro/config_flow.py
+++ custom_components/ha_zmanim_pro/config_flow.py
@@ -1,1 +1,26 @@
+```python id="rdbsnn"
+from __future__ import annotations
 
+import voluptuous as vol
+
+from homeassistant import config_entries
+
+from .const import DOMAIN
+
+
+class ZmanimProConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+
+        if user_input is not None:
+            return self.async_create_entry(
+                title="Zmanim Pro",
+                data=user_input,
+            )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({}),
+        )
+```
