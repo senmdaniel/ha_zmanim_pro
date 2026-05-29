c1ec30049befd45388fe46694c2ce65026f5b49a
senmdaniel <senmdaniel@hotmail.com>
2026-05-29 12:01:18 +0200

    Update config_flow.py

--- custom_components/ha_zmanim_pro/config_flow.py
+++ custom_components/ha_zmanim_pro/config_flow.py
@@ -1,10 +1,9 @@
+```python id="mjlwm8"
 from homeassistant import config_entries
 
 
-class ZmanimProConfigFlow(
-    config_entries.ConfigFlow,
-    domain="ha_zmanim_pro",
-):
+class ConfigFlow(config_entries.ConfigFlow, domain="ha_zmanim_pro"):
+
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
@@ -13,3 +12,5 @@ class ZmanimProConfigFlow(
             title="HA Zmanim Pro",
             data={},
         )
+```
+
